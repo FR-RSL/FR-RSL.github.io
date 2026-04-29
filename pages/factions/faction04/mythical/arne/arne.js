fcc202630b718012e1ba51e22e5462387e581d12
PGBmax <143847076+PGBmax@users.noreply.github.com>
2026-04-29 20:19:07 +0200

    edit: Modifier arne.js (Arne le Blanc)

--- pages/factions/faction04/mythical/arne/arne.js
+++ pages/factions/faction04/mythical/arne/arne.js
@@ -5,9 +5,9 @@ const championForms = {
         {
           img: "assets/sort1.png",
           name: "Chant d'acier",
-          description: `Attaque un ennemi. Place une frappe supplementaire si la cible se trouve sous debuff ${DEBUFFS.DEATHBRAND}.
+          description: `Attaque un ennemi. Place une frappe supplementaire si la cible se trouve sous debuff ${DEBUFFS.DEATHBRAND}.<br><br>
 
-Reduit les PV et la DEF de la cible de 10% (s'accumule jusqu'a 30%).
+Reduit les PV et la DEF de la cible de 10% (s'accumule jusqu'a 30%).<br>
 Cet effet ne fonctionne pas contre les Boss.`,
           damage: "4*ATQ",
           levelInfo: ["Degats +20%"],
@@ -15,9 +15,9 @@ Cet effet ne fonctionne pas contre les Boss.`,
         {
           img: "assets/sort2.png",
           name: "Rage incandescente",
-          description: `Attaque un ennemi. Avant d'attaquer, place un debuff ${DEBUFFS.LOCK_PASSIF} pendant 1 tour. Ignorera 50% de la DEF de la cible, ainsi que les buffs ${BUFFS.BLOCK_DAMAGE} et ${BUFFS.UNKILLABLE}.
+          description: `Attaque un ennemi. Avant d'attaquer, place un debuff ${DEBUFFS.LOCK_PASSIF} pendant 1 tour. Ignorera 50% de la DEF de la cible, ainsi que les buffs ${BUFFS.BLOCK_DAMAGE} et ${BUFFS.UNKILLABLE}.<br><br>
 
-Si la cible initiale est tuee, repetera l'attaque contre tous les ennemis et ignorera 25% de la DEF de chaque cible.
+Si la cible initiale est tuee, repetera l'attaque contre tous les ennemis et ignorera 25% de la DEF de chaque cible.<br>
 Reduira egalement de 2 tours le temps de recharge de la competence <span class='gt'>Hurlement polaire</span> si la cible initiale est tuee lorsqu'elle se trouve sous debuff ${DEBUFFS.DEATHBRAND}.`,
           damage: "6*ATQ",
           cooldown: 4,
@@ -26,9 +26,9 @@ Reduira egalement de 2 tours le temps de recharge de la competence <span class='
         {
           img: "assets/sort3.png",
           name: "Hurlement polaire",
-          description: `Place un buff ${BUFFS.ATK} de 50% et un buff ${BUFFS.PRE} de 50% sur tous les allies pendant 3 tours.
+          description: `Place un buff ${BUFFS.ATK} de 50% et un buff ${BUFFS.PRE} de 50% sur tous les allies pendant 3 tours.<br><br>
 
-Place un debuff ${DEBUFFS.DEATHBRAND} sur tous les ennemis pendant 2 tours.
+Place un debuff ${DEBUFFS.DEATHBRAND} sur tous les ennemis pendant 2 tours.<br><br>
 
 Accorde ensuite un Tour supplementaire.`,
           cooldown: 5,
@@ -45,10 +45,10 @@ Accorde ensuite un Tour supplementaire.`,
           name: "Volonte de la toundra [P]",
           description: `${PASSIVE}
 
-Les ennemis sous debuff ${DEBUFFS.DEATHBRAND} recoivent 20% de degats en plus et infligent 20% de degats en moins.
-Si plusieurs Champions de l'equipe disposent de cette Competence, une seule sera activee. Cette competence ne s'activera pas sur les copies en double de ce Champion si ce Champion specifique est mort.
+Les ennemis sous debuff ${DEBUFFS.DEATHBRAND} recoivent 20% de degats en plus et infligent 20% de degats en moins.<br>
+Si plusieurs Champions de l'equipe disposent de cette Competence, une seule sera activee. Cette competence ne s'activera pas sur les copies en double de ce Champion si ce Champion specifique est mort.<br><br>
 
-Immunise contre les debuffs ${DEBUFFS.GEL}, ${DEBUFFS.STUN} et ${DEBUFFS.PROVOKE}.
+Immunise contre les debuffs ${DEBUFFS.GEL}, ${DEBUFFS.STUN} et ${DEBUFFS.PROVOKE}.<br><br>
 
 ${ACTIVE}
 
@@ -74,7 +74,7 @@ Des que ce Champion est tue, le ranime avec 50% de PV et 100% de Compteur de Tou
         {
           img: "assets/sort_f2_1.png",
           name: "Tempete d'epees",
-          description: `Attaque 2 fois un ennemi. Place un debuff ${DEBUFFS.STUN} pendant 1 tour.
+          description: `Attaque 2 fois un ennemi. Place un debuff ${DEBUFFS.STUN} pendant 1 tour.<br><br>
 
 Si les PV MAX de ce Champion sont superieurs ou egaux a ceux de la cible, il est impossible de resister a ce debuff.`,
           damage: "0.12*HP + 1.4*ATQ",
@@ -83,7 +83,7 @@ Si les PV MAX de ce Champion sont superieurs ou egaux a ceux de la cible, il est
         {
           img: "assets/sort_f2_2.png",
           name: "Rugissement provoquant",
-          description: `Place un debuff ${DEBUFFS.PROVOKE} sur tous les ennemis pendant 1 tour. Si la cible est un Boss, place au lieu de ca un debuff ${DEBUFFS.PROVOKE} pendant 2 tours. Si les PV MAX de ce Champion sont superieurs ou egaux a ceux de la cible, il est impossible de resister a ces debuffs.
+          description: `Place un debuff ${DEBUFFS.PROVOKE} sur tous les ennemis pendant 1 tour. Si la cible est un Boss, place au lieu de ca un debuff ${DEBUFFS.PROVOKE} pendant 2 tours. Si les PV MAX de ce Champion sont superieurs ou egaux a ceux de la cible, il est impossible de resister a ces debuffs.<br><br>
 
 Place un buff ${BUFFS.STRENGTHEN} de 25% sur tous les allies pendant 2 tours. Place egalement un buff ${BUFFS.COUNTER} sur ce Champion pendant 2 tours.`,
           cooldown: 4,
@@ -92,10 +92,10 @@ Place un buff ${BUFFS.STRENGTHEN} de 25% sur tous les allies pendant 2 tours. Pl
         {
           img: "assets/sort_f2_3.png",
           name: "Rupture glaciale",
-          description: `Attaque tous les ennemis. Avant d'attaquer, place un debuff ${DEBUFFS.DEF} de 60% sur tous les ennemis pendant 2 tours. Si les PV MAX de ce Champion sont superieurs ou egaux a ceux de la cible, il est impossible de resister a ce debuff.
+          description: `Attaque tous les ennemis. Avant d'attaquer, place un debuff ${DEBUFFS.DEF} de 60% sur tous les ennemis pendant 2 tours. Si les PV MAX de ce Champion sont superieurs ou egaux a ceux de la cible, il est impossible de resister a ce debuff.<br><br>
 
-Cette attaque inflige a chaque cible des degats individuelle des degats sur cible unique plutot que des degats de zone. Ignorera 30% de la DEF de chaque cible.
-Place une frappe supplementaire sur les Boss et les ennemis sous debuffs ${DEBUFFS.STUN} et ${DEBUFFS.PROVOKE}.
+Cette attaque inflige a chaque cible des degats individuelle des degats sur cible unique plutot que des degats de zone. Ignorera 30% de la DEF de chaque cible.<br>
+Place une frappe supplementaire sur les Boss et les ennemis sous debuffs ${DEBUFFS.STUN} et ${DEBUFFS.PROVOKE}.<br>
 Si une cible est un Boss, ou se trouve sous debuffs ${DEBUFFS.STUN} ou ${DEBUFFS.PROVOKE}, ignorera au lieu de cela 50% de la DEF.`,
           damage: "0.35*PV + 1.4*ATQ",
           cooldown: 4,
@@ -112,13 +112,13 @@ Si une cible est un Boss, ou se trouve sous debuffs ${DEBUFFS.STUN} ou ${DEBUFFS
           name: "Coeur d'ours [P]",
           description: `${PASSIVE}
 
-Augmente les PV MAX et la DEF de ce Champion de 20% pour chaque ennemi et allie mort, dont les Boss et leurs sibres, lors d'un combat (s'accumule jusqu'a 100%).
+Augmente les PV MAX et la DEF de ce Champion de 20% pour chaque ennemi et allie mort, dont les Boss et leurs sibres, lors d'un combat (s'accumule jusqu'a 100%).<br><br>
 
-Immunise contre les debuffs ${DEBUFFS.GEL}, ${DEBUFFS.STUN} et ${DEBUFFS.PROVOKE}.
+Immunise contre les debuffs ${DEBUFFS.GEL}, ${DEBUFFS.STUN} et ${DEBUFFS.PROVOKE}.<br><br>
 
 ${ACTIVE}
 
-Des qu'un ennemi essaie de placer un debuff ${DEBUFFS.GEL}, ${DEBUFFS.STUN} ou ${DEBUFFS.PROVOKE} sur ce Champion, active instantanement la competence <span class='gt'>Rupture Glaciale</span>.
+Des qu'un ennemi essaie de placer un debuff ${DEBUFFS.GEL}, ${DEBUFFS.STUN} ou ${DEBUFFS.PROVOKE} sur ce Champion, active instantanement la competence <span class='gt'>Rupture Glaciale</span>.<br>
 Ceci ne placera pas la competence <span class='gt'>Rupture Glaciale</span> en temps de recharge.`,
           cooldown: 2,
           isPassive: true,

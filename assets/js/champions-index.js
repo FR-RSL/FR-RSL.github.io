1f533ef05246658c2b54d1114d5da47a87acc35b
PGBmax <143847076+PGBmax@users.noreply.github.com>
2026-05-03 22:56:17 +0200

    feat: Ajouter le champion Rae

--- assets/js/champions-index.js
+++ assets/js/champions-index.js
@@ -174,6 +174,7 @@ const CHAMPIONS = [
     { name: 'Vestele epine-fleuve', faction: 'faction15', rarity: 'legendary', folder: 'vestele' },
     { name: 'Baerd le Trapu', faction: 'faction15', rarity: 'legendary', folder: 'baerd' },
     { name: 'Arachoa Tourne-lune', faction: 'faction01', rarity: 'mythical', folder: 'arachoa' },
+    { name: 'Rae', faction: 'faction11', rarity: 'legendary', folder: 'rae' },
   ].map(c => ({
   ...c,
   url: `pages/factions/${c.faction}/${c.rarity}/${c.folder}/${c.folder}.html`,
---
+++ pages/factions/faction11/legendary/rae/rae.js
@@ -0,0 +1,44 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.png",
+          name: "Corrosion",
+          description: `Attaque tous les ennemis. Possede 30% de chances de placer un debuff ${DEBUFFS.DEF} de 60% sur tous les ennemis pendant 2 tours.`,
+          damage: "2.5*ATQ",
+          levelInfo: ["Dégâts +5%", "Dégâts +5%", "Dégâts +10%", "Chances de Buff/Debuff +10%", "Chances de Buff/Debuff +10%"],
+        },
+        {
+          img: "assets/sort2.png",
+          name: "Vagues Arcaniques",
+          description: `Attaque tous les ennemis. Inflige 50% de degats supplementaires contre les cibles affligees de debuffs.`,
+          damage: "4.3*ATQ",
+          cooldown: 4,
+          levelInfo: ["Dégâts +5%", "Dégâts +10%", "Dégâts +15%"],
+        },
+        {
+          img: "assets/sort3.png",
+          name: "Bannissement",
+          description: `Attaque tous les ennemis. Retire tous les buffs dont disposent les ennemis.${RETURN}${RETURN}
+Possede 50% de chances de placer un debuff ${DEBUFFS.GEL} sur tous les ennemis pendant 1 tour.`,
+          damage: "5.3*ATQ",
+          cooldown: 7,
+          levelInfo: ["Dégâts +10%", "Chances de Buff/Debuff +10%", "Chances de Buff/Debuff +10%", "Temps de recharge -1"],
+        }
+  ],
+  stats: {
+        "PV": "15 195",
+        "ATQ": "1 343",
+        "DEF": "1 222",
+        "VIT": "98",
+        "TAUX C.": "15%",
+        "DEG C.": "63%",
+        "RES": "40",
+        "PRE": "0"
+  }
+};
+
+const aura = {
+  img: "../../../../../assets/images/auras/speed.png",
+  description: `Augmente la statistique VIT des Allies dans les Donjons de 33%.`
+};

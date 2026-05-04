bb63876227cbe31053c8e9a9631a3f515a14518b
PGBmax <143847076+PGBmax@users.noreply.github.com>
2026-05-04 02:58:02 +0200

    feat: Ajouter le champion maranix

---
+++ pages/factions/faction11/legendary/maranix/maranix.js
@@ -0,0 +1,49 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.png",
+          name: "Du de la Faucheuse",
+          description: `Attaque 2 fois un ennemi. Possede 50% de chances de placer 2 frappes supplementaires si la cible se trouve sous debuff ${DEBUFFS.HEX}. Les chances de placer une frappe supplementaire sont calculees individuellement par frappe.`,
+          damage: "1.9*ATQ",
+          levelInfo: ["Dégâts +5%", "Dégâts +5%", "Dégâts +5%", "Dégâts +5%"],
+        },
+        {
+          img: "assets/sort2.png",
+          name: "Torturer",
+          description: `Attaque tous les ennemis. Possede 75% de chances de placer un debuff ${DEBUFFS.HEX} et un debuff ${DEBUFFS.DEF} de 60% pendant 2 tours.`,
+          damage: "4*ATQ",
+          cooldown: 4,
+          levelInfo: ["Dégâts +10%", "Dégâts +10%", "Chances de Buff/Debuff +10%", "Chances de Buff/Debuff +15%", "Temps de recharge -1"],
+        },
+        {
+          img: "assets/sort3.png",
+          name: "Fin inexorable",
+          description: `Attaque tous les ennemis. Ignorera les buffs ${BUFFS.UNKILLABLE} et ${BUFFS.SHIELD} si les cibles se trouvent sous debuff ${DEBUFFS.HEX}.${RETURN}${RETURN}
+Place un buff ${BUFFS.UNKILLABLE} sur cette Championne pendant 1 tour si cette attaque tue un ennemi sans debuff ${DEBUFFS.HEX}.
+Place un buff ${BUFFS.UNKILLABLE} pendant 2 tours si cette attaque tue un ennemi sous debuff ${DEBUFFS.HEX}.`,
+          damage: "4.1*ATQ",
+          cooldown: 6,
+          levelInfo: ["Dégâts +10%", "Temps de recharge -1", "Temps de recharge -1"],
+        },
+        {
+          img: "assets/sort4.png",
+          name: "Agente de la Mort [P]",
+          description: `Reinitialise le temps de recharge de la competence <span class="grn-t">[Fin inexorable]</span> des que cette Championne tue un ennemi sous debuff ${DEBUFFS.HEX}.${RETURN}${RETURN}
+Augmente l'ATQ de cette Championne de 5% pour chaque ennemi sous debuff ${DEBUFFS.HEX}.`,
+          isPassive: true,
+        }
+  ],
+  stats: {
+        "PV": "15 360",
+        "ATQ": "1 487",
+        "DEF": "1 068",
+        "VIT": "100",
+        "TAUX C.": "15%",
+        "DEG C.": "63%",
+        "RES": "30",
+        "PRE": "10"
+  }
+};
+
+const aura = null;

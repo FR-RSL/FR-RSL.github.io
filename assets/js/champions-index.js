9e95a213794b208dd20dca085a73713a8acd5a87
PGBmax <143847076+PGBmax@users.noreply.github.com>
2026-05-16 19:04:17 +0200

    feat: Ajouter le champion Moissonneuse

--- assets/js/champions-index.js
+++ assets/js/champions-index.js
@@ -207,6 +207,7 @@ const CHAMPIONS = [
     { name: "Domestique", faction: 'faction11', rarity: 'rare', folder: 'domestique' },
     { name: "Evisceratrice", faction: 'faction11', rarity: 'rare', folder: 'evisceratrice' },
     { name: "Garde-Douleur", faction: 'faction11', rarity: 'rare', folder: 'gardedouleur' },
+    { name: "Moissonneuse", faction: 'faction11', rarity: 'rare', folder: 'moissonneuse' },
   ].map(c => ({
   ...c,
   url: `pages/factions/${c.faction}/${c.rarity}/${c.folder}/${c.folder}.html`,
---
+++ pages/factions/faction11/rare/moissonneuse/moissonneuse.js
@@ -0,0 +1,42 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.png",
+          name: "Destabilise",
+          description: `Attaque 2 fois un ennemi.${RETURN}${RETURN} Possede 35% de chances de placer un debuff ${DEBUFFS.WEAKEN} de 15% pendant 2 tours.`,
+          damage: "1.9*ATQ",
+          levelInfo: ["Dégâts +5%", "Dégâts +5%", "Chances de Buff/Debuff +5%", "Dégâts +5%", "Chances de Buff/Debuff +10%"],
+        },
+        {
+          img: "assets/sort2.png",
+          name: "Faux Maudite",
+          description: `Attaque un ennemi.${RETURN}${RETURN}
+Possede 80% de chances de placer un debuff ${DEBUFFS.BLOCK_BUFFS} pendant 2 tours.`,
+          damage: "6.5*ATQ",
+          cooldown: 3,
+          levelInfo: ["Dégâts +5%", "Dégâts +10%", "Chances de Buff/Debuff +5%", "Chances de Buff/Debuff +5%", "Chances de Buff/Debuff +10%"],
+        },
+        {
+          img: "assets/sort3.png",
+          name: "Salve Temporelle",
+          description: `Attaque un ennemi.${RETURN}${RETURN}
+Possede 90% de chances d'augmenter de 2 tours le temps de recharge d'une des Competences de la cible prise au hasard.`,
+          damage: "7.5*ATQ",
+          cooldown: 5,
+          levelInfo: ["Chances de Buff/Debuff +5%", "Dégâts +10%", "Chances de Buff/Debuff +5%", "Temps de recharge -1"],
+        }
+  ],
+  stats: {
+        "PV": "13 710",
+        "ATQ": "1 156",
+        "DEF": "958",
+        "VIT": "100",
+        "TAUX C.": "15%",
+        "DEG C.": "57%",
+        "RES": "30",
+        "PRE": "0"
+  }
+};
+
+const aura = null;

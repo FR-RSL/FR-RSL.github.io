639deeae235408222d552c45fbd49eaef47fc9d2
PGBmax <levinounet@gmail.com>
2026-05-31 05:06:43 +0200

    refresh: common/preacher/preacher.js

---
+++ pages/factions/faction03/common/preacher/preacher.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "PV",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Holy Blow",
+          description: `Attacks 1 enemy. Has a 25% chance of placing a 25% [Decrease ATK] debuff for 1 turn.`,
+          damage: "3.5*ATK",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Damage +5%", "Level 5: Damage +5%", "Level 6: Damage +5%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "10 080",
+        "ATQ": "837",
+        "DEF": "694",
+        "VIT": "96",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

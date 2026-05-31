f088f4eb1419c6a86b49da39bcd3975a103812b2
PGBmax <levinounet@gmail.com>
2026-05-31 05:07:08 +0200

    data: common/bully/bully.js

---
+++ pages/factions/faction04/common/bully/bully.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Défense",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Bullying",
+          description: `Attacks 1 enemy.`,
+          damage: "2.3*DEF",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Damage +5%", "Level 5: Damage +5%", "Level 6: Damage +5%", "Level 7: Damage +5%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "11 565",
+        "ATQ": "462",
+        "DEF": "969",
+        "VIT": "91",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

428825704e04e78b9bc14181971247b8ae066921
PGBmax <levinounet@gmail.com>
2026-05-31 05:11:23 +0200

    update: mythical/arbais/arbais.js

--- pages/factions/faction15/mythical/arbais/arbais.js
+++ pages/factions/faction15/mythical/arbais/arbais.js
@@ -1,170 +1,115 @@
-// Données des deux formes de Toshiro
 const championForms = {
   form1: {
-        type: "Soutien",
-        spells: [
-          {
-            img: "assets/sort1.webp",
-            name: "Lame-ouragan",
-            description: `
-              Attaque tous les ennemis.
-              Possède 40% de chances de placer un débuff
-              <span class='gbt'>Blocage des compétences Actives</span> pendant 1 tour.
-            `,
-            damage: "3.75*ATQ",
-            levelInfo: [
-              "Dégâts +10%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort2.webp",
-            name: "Renaissance verdoyante",
-            description: `
-              Retire tous les buffs dont disposent les ennemis.
-              Augmente d'1 tour la durée de tous les buffs alliés.<br><br>
-              Place ensuite un buff <span class='gbt'>Ranimer apres la Mort</span>
-              sur tous les alliés pendant 2 tours.
-            `,
-            cooldown: 4,
-            levelInfo: [
-              "Ignorer la RES +20%",
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/sort3.webp",
-            name: "Murmure harmonieux",
-            description: `
-              Remplit le Compteur de Tour de tous les alliés de 30%,
-              puis place un buff <span class='gbt'>Augmentation d'ATQ</span> de 50% 
-              et un buff <span class='gbt'>Augmentation de PRE</span> de 50%
-              sur tous les alliés pendant 2 tours.
-            `,
-            cooldown: 5,
-            levelInfo: [
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/meta1.webp",
-            name: "Metamorphe",
-            description: `
-              Fait passer ce Champion a sa Forme alternative.
-              Accorde ensuite un Tour supplémentaire.
-            `,
-            cooldown: 4,
-          },
-          {
-            img: "assets/passif1.webp",
-            name: "Etreinte de Pierrepine [P]",
-            description: `
-              Des qu'un allié disposant d'un buff <span class='gbt'>Ranimer apres la Mort</span>
-              place par cette Championne se fait attaquer, Possède 30% de chances
-              de placer un débuff <span class='gbt'>Petrification</span>
-              sur l'assaillant pendant 1 tour.
-            `,
-            isPassive: true,
-          },
-        ],
-        stats: {
-          "PV": "22 305",
-          "ATQ": "980",
-          "DEF": "1 387",
-          "VIT": "115",
-          "TAUX C.": "15%",
-          "DÉG C.": "50%",
-          "RÉS": "30",
-          "PRÉ": "20"
-        },
-      },
-      form2: {
-        type: "PV",
-        spells: [
+    type: "Soutien",
+    spells: [
         {
-            img: "assets/sort4.webp",
-            name: "Lance-rocher",
-            description: `
-              Attaque un ennemi. Reduit le Compteur de Tour de la cible de 30%.<br><br>
-              Place ensuite un buff <span class='gbt'>Soins continus</span> de 15%
-              sur cette Championne pendant 1 tour.
-              place également un buff <span class='gbt'>Soins continus</span> de 15%
-              sur l'allié ayanat le moins de PV, sauf cette Championne, pendant 1 tour.
-            `,
-            damage: "0.27*PV",
-            levelInfo: [
-              "Dégâts +10%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort5.webp",
-            name: "Bruleronce",
-            description: `
-              Attaque tous les ennemis. 
-              Place un débuff <span class='gbt'>Sangsue</span>
-              et un débuff <span class='gbt'>Brulure de PV</span> pendant 2 tours.
-            `,
-            damage: "0.25*PV",
-            cooldown: 3,
-            levelInfo: [
-              "Dégâts +10%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort6.webp",
-            name: "Clameur du Bois de Brume",
-            description: `
-              Place deux buffs <span class='gbt'>Soins continus</span> de 15%
-              sur tous les alliés pendant 2 tours.<br><br>
-              Place également un buff <span class='gbt'>Raillerie</span>
-              sur cette Championne pendant 2 tours, ainsi qu'un buff
-              <span class='gbt'>Peau de Pierre</span> pendant 1 tour.
-            `,
-            cooldown: 5,
-            levelInfo: [
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/meta2.webp",
-            name: "Metamorphe",
-            description: `
-              Fait passer ce Champion a sa Forme de base.
-              Accorde ensuite un Tour supplémentaire.
-            `,
-            cooldown: 4,
-          },
-          {
-            img: "assets/passif2.webp",
-          name: "Fouillis d'epine [P]",
-          description: `
-              Lorsqu'elle est attaquee, place un débuff
-              <span class='gbt'>Reduction de VIT</span> de 30% sur l'assaillant pendant 2 tours.
-              Se produit une fois par frappe.<br><br>
-              Reduit également de 20% les degats infliges par des compétences pour tous les alliés.
-              Cette Championne recevra ces degats a la place.
-            `,
-            isPassive: true,
-          },
-        ],
-        stats: {
-          "PV": "23 955",
-          "ATQ": "958",
-          "DEF": "1 299",
-          "VIT": "100",
-          "TAUX C.": "15%",
-          "DÉG C.": "50%",
-          "RÉS": "50",
-          "PRÉ": "20"
+          img: "assets/sort1.webp",
+          name: "Bladegale",
+          description: `Attacks all enemies. Has a 40% chance of placing a [Block Active Skills] debuff for 1 turn.`,
+          damage: "3.75*ATK",
+          levelInfo: ["Level 2: Damage +10%", "Level 3: Ignore RES +20%"],
+          isPassive: false
         },
+        {
+          img: "assets/sort2.webp",
+          name: "Verdant Rebirth",
+          description: `Removes all buffs from all enemies. Increases the duration of all ally buffs by 1 turn. <br><br>Then places a [Revive On Death] buff on all allies for 2 turns.`,
+          cooldown: 4,
+          levelInfo: ["Level 2: Ignore RES +20%", "Level 3: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort3.webp",
+          name: "Harmonious Whisper",
+          description: `Fills the Turn Meters of all allies by 30% and places a 50% [Increase ATK] buff and a 50% [Increase ACC] buff on all allies for 2 turns.`,
+          cooldown: 5,
+          levelInfo: ["Level 2: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/meta1.webp",
+          name: "Metamorph",
+          description: `Transforms this Champion into their Alternate Form. Then grants an Extra Turn.`,
+          cooldown: 4,
+          isPassive: false
+        },
+        {
+          img: "assets/passif1.webp",
+          name: "Stonethorn's Embrace [P]",
+          description: `Has a 30% chance of placing a [Petrification] debuff on an attacker for 1 turn, whenever an ally is attacked while under a [Revive On Death] buff placed by this Champion.`,
+          isPassive: true
+        }
+      ],
+    stats: {
+        "PV": "22 305",
+        "ATQ": "980",
+        "DEF": "1 387",
+        "VIT": "115",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "20"
       },
-    };
-
-    const aura = {
-      img: "../../../../../../assets/images/auras/speed.webp",
-      description: `
-        Augmente la statistique VIT des Alliés lors de toutes les Batailles de 25%.
-      `,
-    };
+    aura: {
+    img: "../../../../../assets/images/auras/speed.webp",
+    description: `Increases Ally SPD in All Battles by 25%`,
+  },
+  },
+  form2: {
+    type: "PV",
+    spells: [
+        {
+          img: "assets/sort4.webp",
+          name: "Boulder Hurl",
+          description: `Attacks 1 enemy. Decreases the target’s Turn Meter by 30%. <br><br>Then places a 15% [Continuous Heal] buff on this Champion for 1 turn. Also places a 15% [Continuous Heal] buff on the ally with the lowest HP except this Champion for 1 turn.`,
+          damage: "0.27*HP",
+          levelInfo: ["Level 2: Damage +10%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort5.webp",
+          name: "Briarburn",
+          description: `Attacks all enemies. Places a [Leech] debuff and a [HP Burn] debuff for 2 turns.`,
+          damage: "0.25*HP",
+          cooldown: 3,
+          levelInfo: ["Level 2: Damage +10%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort6.webp",
+          name: "Roar of the Mistwood",
+          description: `Places two 15% [Continuous Heal] buffs on all allies for 2 turns. <br><br>Also places a [Taunt] buff on this Champion for 2 turns, as well as a [Stone Skin] buff for 1 turn.`,
+          cooldown: 5,
+          levelInfo: ["Level 2: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/meta2.webp",
+          name: "Metamorph",
+          description: `Transforms this Champion into their Base Form. Then grants an Extra Turn.`,
+          cooldown: 4,
+          isPassive: false
+        },
+        {
+          img: "assets/passif2.webp",
+          name: "Tanglethorn [P]",
+          description: `When attacked, places a 30% [Decrease SPD] debuff on the attacker for 2 turns. Occurs once per hit. <br><br>Also decreases the damage taken by all allies from skills by 20%. This Champion will receive that damage instead.`,
+          isPassive: true
+        }
+      ],
+    stats: {
+        "PV": "23 955",
+        "ATQ": "958",
+        "DEF": "1 299",
+        "VIT": "100",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "50",
+        "PRÉ": "20"
+      },
+    aura: {
+    img: "../../../../../assets/images/auras/speed.webp",
+    description: `Increases Ally SPD in All Battles by 25%`,
+  },
+  },
+};

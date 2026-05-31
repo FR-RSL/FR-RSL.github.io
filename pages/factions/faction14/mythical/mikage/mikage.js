054cffcb4eee494f07bd9f4fb919c14997aa22cf
PGBmax <levinounet@gmail.com>
2026-05-31 05:11:16 +0200

    chore: mythical/mikage/mikage.js

--- pages/factions/faction14/mythical/mikage/mikage.js
+++ pages/factions/faction14/mythical/mikage/mikage.js
@@ -1,167 +1,117 @@
-// Données des deux formes de Toshiro
 const championForms = {
   form1: {
-        type: "Soutien",
-        spells: [
-          {
-            img: "assets/sort1.webp",
-            name: "Fouet fantome",
-            description: `
-              Attaque un ennemi avec un allié aleatoire de la Faction du Clan de l'Ombre
-              aleatoire. L'allié qui rejoint l'attaque utilisera sa compétence par defaut.<br><br>
-              Place pendant 2 tours un buff ${BUFFS.SHIELD} equivalent a 10% des PV MAX
-              de cette Championne sur l'allié ayant les PV les plus bas.
-            `,
-            damage: "3.9*ATQ",
-            levelInfo: [
-              "Dégâts +10%"
-            ]
-          },
-          {
-            img: "assets/sort2.webp",
-            name: "Piege de soie",
-            description: `
-              Attaque tous les ennemis. Reduit d'1 tour la durée de tous 
-              les buffs des ennemis et de tous les débuffs des alliés.<br><br>
-              Augmente ensuite d'1 tour la durée de tous les débuffs des ennemis
-              et de tous les buffs des alliés.
-            `,
-            damage: "3.8*ATQ",
-            cooldown: 3,
-            levelInfo: [
-              "Dégâts +10%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort3.webp",
-            name: "Decret imperial",
-            description: `
-              Place un buff <span class='gbt'>Augmentation d'ATQ</span> de 50%
-              et un buff <span class='gbt'>Augmentation des DEG C.</span> de 30% sur tous les alliés pendant 2 tours,
-              puis fait equipe avec eux pour attaquer un seul ennemi.
-              Les alliés qui rejoignent l'attaque utilisent leurs compétences par defaut.
-            `,
-            cooldown: 5,
-            levelInfo: [
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/meta1.webp",
-            name: "Metamorphe",
-            description: `
-              Fait passer ce Champion a sa Forme alternative.
-              Accorde ensuite un Tour supplémentaire.
-            `,
-            cooldown: 4,
-          },
-          {
-            img: "assets/passif1.webp",
-            name: "Volonte de Mikage [P]",
-            description: `
-              Au debut du tour de cette Championne,
-              retire tous les débuffs sur l'allié ayant l'ATQ la plus elevee.
-            `,
-            isPassive: true,
-          },
-        ],
-        stats: {
-          "PV": "21 645",
-          "ATQ": "1 200",
-          "DEF": "1 211",
-          "VIT": "115",
-          "TAUX C.": "15%",
-          "DÉG C.": "50%",
-          "RÉS": "30",
-          "PRÉ": "20"
-        },
-      },
-      form2: {
-        type: "Soutien",
-        spells: [
+    type: "Soutien",
+    spells: [
         {
-            img: "assets/sort4.webp",
-            name: "Araignee de cauchemar",
-            description: `
-              Attaque un ennemi.
-              Place un débuff <span class='gbt'>Sommeil</span> pendant 1 tour.<br><br>
-              Reduit également le Compteur de Tour de la cible de 15%.
-            `,
-            damage: "3.9*ATQ",
-            levelInfo: [
-              "Dégâts +10%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort5.webp",
-            name: "Mort fuyante",
-            description: `
-              Attaque tous les ennemis. 
-              Place un débuff <span class='gbt'>Etourdissement</span> pendant 1 tour.<br><br>
-              Reduit également le Compteur de Tour de tous les ennemis de 30%.
-            `,
-            damage: "3.8*ATQ",
-            cooldown: 3,
-            levelInfo: [
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort6.webp",
-            name: "Ruine de Yakai",
-            description: `
-              Place un buff <span class='gbt'>Augmentation de PRE</span> de 50% 
-              sur tous les alliés pendant 2 tours.<br><br>
-              Retire également tous les buffs sur tous les ennemis, 
-              puis place un débuff <span class='gbt'>Affaiblissement</span> 
-              de 25% pendant 2 tours.
-            `,
-            cooldown: 5,
-            levelInfo: [
-              "Ignorer la RES +20%",
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/meta2.webp",
-            name: "Metamorphe",
-            description: `
-              Fait passer ce Champion a sa Forme de base.
-              Accorde ensuite un Tour supplémentaire.
-            `,
-            cooldown: 4,
-          },
-          {
-            img: "assets/passif2.webp",
-          name: "Regarde demoniaque [P]",
-          description: `
-              Au debut du tour de cette Championne, reduit de 15%
-              le Compteur de Tour de l'ennemi ayant l'ATQ la plus elevee.
-            `,
-            isPassive: true,
-            levelInfo: [
-              "Ignorer la RES +20%"
-            ]
-          },
-        ],
-        stats: {
-          "PV": "21 645",
-          "ATQ": "1 200",
-          "DEF": "1 211",
-          "VIT": "115",
-          "TAUX C.": "15%",
-          "DÉG C.": "50%",
-          "RÉS": "30",
-          "PRÉ": "20"
+          img: "assets/sort1.webp",
+          name: "Shadow Lash",
+          description: `Attacks 1 enemy with 1 random ally from the Shadowkin Faction. The ally joining the attack will use their default skill.<br><br>Places a [Shield] buff equal to 10% of this Champion's MAX HP on the ally with the lowest HP for 2 turns.<br><br><br>Shield Multiplier: 0.1*HP`,
+          damage: "3.9*ATK",
+          levelInfo: ["Level 2: Damage +10%"],
+          isPassive: false
         },
+        {
+          img: "assets/sort2.webp",
+          name: "Silkensnare",
+          description: `Attacks all enemies. Decreases the duration of all enemy buffs and all ally debuffs by 1 turn. <br><br>Then, increases the duration of all enemy debuffs and all ally buffs by 1 turn.`,
+          damage: "3.8*ATK",
+          cooldown: 3,
+          levelInfo: ["Level 2: Damage +10%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort3.webp",
+          name: "Imperial Decree",
+          description: `Places a 50% [Increase ATK] buff and a 30% [Increase C. DMG] buff on all allies for 2 turns, then teams up with all allies to attack a single enemy. The allies joining the attack will use their default skills.`,
+          cooldown: 5,
+          levelInfo: ["Level 2: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/meta1.webp",
+          name: "Metamorph",
+          description: `Transforms this Champion into their Alternate Form. Then grants an Extra Turn.`,
+          cooldown: 4,
+          isPassive: false
+        },
+        {
+          img: "assets/passif1.webp",
+          name: "Will of the Mikage [P]",
+          description: `At the start of this Champion’s turn, removes all debuffs from the ally with the highest ATK.`,
+          isPassive: true
+        }
+      ],
+    stats: {
+        "PV": "21 645",
+        "ATQ": "1 200",
+        "DEF": "1 211",
+        "VIT": "115",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "20"
       },
-    };
-
-    const aura = {
-      img: "../../../../../assets/images/auras/acc.webp",
-      description: `
-        Augmente la statistique PRE des Alliés lors de toutes les Batailles de 80.
-      `,
-    };
+    aura: {
+    img: "../../../../../assets/images/auras/acc.webp",
+    description: `Increases Ally ACC in All Battles by 80`,
+  },
+  },
+  form2: {
+    type: "Soutien",
+    spells: [
+        {
+          img: "assets/sort4.webp",
+          name: "Nightmare Spider",
+          description: `Attacks 1 enemy. Places a [Sleep] debuff for 1 turn. <br><br>Also decreases the target’s Turn Meter by 15%.`,
+          damage: "3.9*ATK",
+          levelInfo: ["Level 2: Damage +10%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort5.webp",
+          name: "Skittering Death",
+          description: `Attacks all enemies. Places a [Stun] debuff for 1 turn. <br><br>Also decreases the Turn Meters of all enemies by 30%.`,
+          damage: "3.8*ATK",
+          cooldown: 3,
+          levelInfo: ["Level 2: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort6.webp",
+          name: "Doom of Yakai",
+          description: `Places a 50% [Increase ACC] buff on all allies for 2 turns.<br><br>Also removes all buffs from all enemies, and places a 25% [Weaken] debuff on them for 2 turns.`,
+          cooldown: 5,
+          levelInfo: ["Level 2: Ignore RES +20%", "Level 3: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/meta2.webp",
+          name: "Metamorph",
+          description: `Transforms this Champion into their Base Form. Then grants an Extra Turn.`,
+          cooldown: 4,
+          isPassive: false
+        },
+        {
+          img: "assets/passif2.webp",
+          name: "Demonic Glower [P]",
+          description: `At the start of this Champion’s turn, decreases the Turn Meter of the enemy with the highest ATK by 15%.`,
+          levelInfo: ["Level 2: Ignore RES +20%"],
+          isPassive: true
+        }
+      ],
+    stats: {
+        "PV": "21 645",
+        "ATQ": "1 200",
+        "DEF": "1 211",
+        "VIT": "115",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "20"
+      },
+    aura: {
+    img: "../../../../../assets/images/auras/acc.webp",
+    description: `Increases Ally ACC in All Battles by 80`,
+  },
+  },
+};

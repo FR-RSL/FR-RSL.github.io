d508794591e63d2981767e8222a52bf408069311
PGBmax <levinounet@gmail.com>
2026-05-31 05:06:10 +0200

    generate: common/bandit/bandit.js

---
+++ pages/factions/faction01/common/bandit/bandit.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Flaming Shot",
+          description: `Attacks 1 enemy. Has a 75% chance of placing a 30% [Decrease DEF] debuff for 1 turn.`,
+          damage: "2.5*ATK",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Buff/Debuff Chance +5%", "Level 4: Damage +5%", "Level 5: Buff/Debuff Chance +5%", "Level 6: Damage +10%", "Level 7: Buff/Debuff Chance +10%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "8 595",
+        "ATQ": "980",
+        "DEF": "650",
+        "VIT": "98",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

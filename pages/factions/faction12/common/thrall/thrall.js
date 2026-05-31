7361289e14667fb7b26b69790efda884cc41ac86
PGBmax <levinounet@gmail.com>
2026-05-31 05:10:31 +0200

    refresh: common/thrall/thrall.js

---
+++ pages/factions/faction12/common/thrall/thrall.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Shambling Assault",
+          description: `Attacks 1 enemy. Has a 5% chance of placing an extra hit.`,
+          damage: "4.4*ATK",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Damage +5%", "Level 5: Damage +10%", "Level 6: Buff/Debuff Chance +5%", "Level 7: Buff/Debuff Chance +5%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "9 915",
+        "ATQ": "958",
+        "DEF": "584",
+        "VIT": "97",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

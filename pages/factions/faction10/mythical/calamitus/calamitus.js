078f290589c5b9ee72ba51aa0cf95697e9f81f35
PGBmax <levinounet@gmail.com>
2026-05-31 05:10:15 +0200

    include: mythical/calamitus/calamitus.js

--- pages/factions/faction10/mythical/calamitus/calamitus.js
+++ pages/factions/faction10/mythical/calamitus/calamitus.js
@@ -1,188 +1,118 @@
-// Données des deux formes de Toshiro
 const championForms = {
   form1: {
-        type: "Attaque",
-        spells: [
-          {
-            img: "assets/sort1.webp",
-            name: "Massacre faucheur",
-            description: `
-              Attaque 2 fois un ennemi. Chaque frappe a 20% de chances
-              supplémentaires de porter un coup critique.<br><br>
-              Chaque frappe restaure les PV MAX detruits de ce Champion
-              a hauteur des degats infliges. Chaque frappe soigne également
-              ce Champion a hauteur de 30% des degats infliges.<br><br>
-              Accorde un Tour supplémentaire si cette attaque tue un ennemi.
-            `,
-            damage: "2.1*ATQ",
-            levelInfo: [
-              "Dégâts +20%"
-            ]
-          },
-          {
-            img: "assets/sort2.webp",
-            name: "Horde de calamite",
-            description: `
-              Attaque 4 fois un ennemi. Chaque frappe a 20% de chances
-              supplémentaires de porter un coup critique.<br><br>
-              La premiere frappe ignorera 10% de la DEF de la cible.
-              Chaque frappe suivante ignorera 5% supplémentaires de la DEF de la cible.<br><br>
-              La premiere frappe place un débuff 
-              <span class='gbt'>Blocage des Buffs</span> pendant 2 tours.<br><br>
-              La deuxieme frappe place un débuff 
-              <span class='gbt'>Reduction de DEF</span> de 60% pendant 2 tours.<br><br>
-              La troisieme frappe place un débuff 
-              <span class='gbt'>Affaiblissement</span> de 25% pendant 2 tours.<br><br>
-              La quatrieme frappe place un débuff 
-              <span class='gbt'>Reduction de VIT</span> de 30% pendant 2 tours.
-            `,
-            damage: "1.8*ATQ",
-            cooldown: 3,
-            levelInfo: [
-              "Dégâts +20%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort3.webp",
-            name: "Necroclysme",
-            description: `
-              Attaque tous les ennemis. Cette attaque est toujours critique
-              et ignorera 20% de la DEF de chaque ennemi.<br><br>
-              Si cette attaque tue un ennemi, place un buff 
-              <span class='gbt'>Invincible</span> sur ce Champion pendant 1 tour.<br><br>
-              Si cette attaque tue deux ennemis ou plus, place au lieu de ca un buff
-              <span class='gbt'>Invincible</span> sur ce Champion pendant 2 tours.
-            `,
-            damage: "4.8*ATQ",
-            cooldown: 4,
-            levelInfo: [
-              "Dégâts +20%",
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/meta1.webp",
-            name: "Metamorphe",
-            description: `
-              Fait passer ce Champion a sa Forme alternative.
-              Accorde ensuite un Tour supplémentaire.
-            `,
-            cooldown: 4,
-          },
-          {
-            img: "assets/passif1.webp",
-            name: "Creation immortelle [P]",
-            description: `
-              Au debut du tour de ce Champion, lui accorde un buff
-              <span class='gbt'>Bouclier</span> equivalent a 50% de ses PV MAX pendant 1 tour.
-              Place également un buff <span class='gbt'>Augmentation d'ATQ</span> de 50% 
-              sur ce Champion pendant 1 tour.
-            `,
-            isPassive: true,
-          },
-        ],
-        stats: {
-          "PV": "16 845",
-          "ATQ": "1 553",
-          "DEF": "1 178",
-          "VIT": "105",
-          "TAUX C.": "15%",
-          "DÉG C.": "63%",
-          "RÉS": "30",
-          "PRÉ": "20"
-        },
-      },
-      form2: {
-        type: "Attaque",
-        spells: [
+    type: "Attaque",
+    spells: [
         {
-            img: "assets/sort4.webp",
-            name: "Faux cruelle",
-            description: `
-              Attaque un ennemi. Possède 20% de chances supplémentaires de porter un coup critique.<br><br>
-              Apres l'attaque, transfere tous les débuffs de ce Champion sur la cible.<br><br>
-              Si la cible se trouve sous débuff <span class='gbt'>Malediction</span> avant l'attaque,
-              transfere au lieu de ca tous les débuffs de ce Champion sur la cible avant l'attaque.`,
-            damage: "4*ATQ",
-            levelInfo: [
-              "Dégâts +20%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/sort5.webp",
-            name: "Ultime testament",
-            description: `
-              Attaque tous les ennemis. Possède 20% de chances supplémentaires de porter un coup critique. 
-              Avant d'attaquer, retire tous les débuffs sur ce Champion.<br><br>
-              Ignorera 20% de la DEF de chaque ennemi.
-              Ignorera au lieu ce ca 35% de la DEF de la cible si elle se trouve sous débuff
-              <span class='gbt'>Malediction</span>.<br><br>
-              Les degats augmentent de 10% pour chaque débuff sur chaque cible.
-            `,
-            damage: "4.2*ATQ",
-            cooldown: 4,
-            levelInfo: [
-              "Dégâts +20%",
-              "Temps de recharge -1"
-            ]
-          },
-          {
-            img: "assets/sort6.webp",
-            name: "Extinction",
-            description: `
-              Attaque tous les ennemis. Possède 20% de chances supplémentaires de porter un coup critique.
-              Avant d'attaquer, place un débuff <span class='gbt'>Malediction</span>
-              et un débuff <span class='gbt'>Reduction de DEF</span> de 60% pendant 2 tours.<br><br>
-              Ignorera les buffs <span class='gbt'>Barriere de Vie</span>,
-              <span class='gbt'>Nuage de Poison</span> et 
-              <span class='gbt'>Protection d'allié</span>.
-            `,
-            damage: "4.4*ATQ",
-            cooldown: 3,
-            levelInfo: [
-              "Dégâts +20%",
-              "Ignorer la RES +20%"
-            ]
-          },
-          {
-            img: "assets/meta2.webp",
-            name: "Metamorphe",
-            description: `
-              Fait passer ce Champion a sa Forme de base.
-              Accorde ensuite un Tour supplémentaire.
-            `,
-            cooldown: 4,
-          },
-          {
-            img: "assets/passif2.webp",
-          name: "Banquet de la mort [P]",
-          description: `
-            Des que ce Champion attaque un ennemi sous débuff <span class='gbt'>Malediction</span>,
-            soigne ce Champion de 30% des degats infliges.<br><br>
-            Augmente l'ATQ de ce Champion de 20% pour chaque ennemi sous débuff
-            <span class='gbt'>Malediction</span>.
-          `,
-            isPassive: true,
-          },
-        ],
-        stats: {
-          "PV": "16 845",
-          "ATQ": "1 553",
-          "DEF": "1 178",
-          "VIT": "105",
-          "TAUX C.": "15%",
-          "DÉG C.": "63%",
-          "RÉS": "30",
-          "PRÉ": "20"
+          img: "assets/sort1.webp",
+          name: "Reaping Cull",
+          description: `Attacks 1 enemy 2 times. Each hit has an extra 20% chance of inflicting a critical hit.<br><br>Each hit restores this Champion's destroyed MAX HP, equal to the damage inflicted. Each hit also heals this Champion by 30% of the damage inflicted. <br><br>Grants an Extra Turn if this attack kills an enemy.<br><br><br>Heal Multiplier: 0.3*Damage Dealt`,
+          damage: "2.1*ATK",
+          levelInfo: ["Level 2: Damage +20%"],
+          isPassive: false
         },
+        {
+          img: "assets/sort2.webp",
+          name: "Horde of Calamity",
+          description: `Attacks 1 enemy 4 times. Each hit has an extra 20% chance of inflicting a critical hit.<br><br>The first hit will ignore 10% of the target's DEF. Each subsequent hit will ignore an additional 5% of the target's DEF.<br><br>The first hit places a [Block Buffs] debuff for 2 turns. <br><br>The second hit places a 60% [Decrease DEF] debuff for 2 turns. <br><br>The third hit places a 25% [Weaken] debuff for 2 turns. <br><br>The fourth hit places a 30% [Decrease SPD] debuff for 2 turns.`,
+          damage: "1.8*ATK",
+          cooldown: 3,
+          levelInfo: ["Level 2: Damage +20%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort3.webp",
+          name: "Necroclysm",
+          description: `Attacks all enemies. This attack is always critical, and will ignore 20% of each enemy's DEF.<br><br>If this attack kills an enemy, places an [Unkillable] buff on this Champion for 1 turn. <br><br>If this attack kills 2 or more enemies, places an [Unkillable] buff on this Champion for 2 turns instead.`,
+          damage: "4.8*ATK",
+          cooldown: 5,
+          levelInfo: ["Level 2: Damage +20%", "Level 3: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/meta1.webp",
+          name: "Metamorph",
+          description: `Transforms this Champion into their Alternate Form. Then grants an Extra Turn.`,
+          cooldown: 4,
+          isPassive: false
+        },
+        {
+          img: "assets/passif1.webp",
+          name: "Immortal Creation [P]",
+          description: `At the start of this Champion's turn, places a [Shield] buff equal to 50% of this Champion's MAX HP on them for 1 turn. Also places a 50% [Increase ATK] buff on this Champion for 1 turn.<br><br>Shield Multiplier: 0.5*HP`,
+          isPassive: true
+        }
+      ],
+    stats: {
+        "PV": "16 845",
+        "ATQ": "1 553",
+        "DEF": "1 178",
+        "VIT": "105",
+        "TAUX C.": "15%",
+        "DÉG C.": "63%",
+        "RÉS": "30",
+        "PRÉ": "20"
       },
-    };
-
-    const aura = {
-      img: "../../../../../../assets/images/auras/attack.webp",
-      description: `
-        Augmente la statistique ATQ des Alliés lors de toutes les Batailles de 35%.
-      `,
-    };
+    aura: {
+    img: "../../../../../assets/images/auras/attack.webp",
+    description: `Increases Ally ATK in All Battles by 35%`,
+  },
+  },
+  form2: {
+    type: "Attaque",
+    spells: [
+        {
+          img: "assets/sort4.webp",
+          name: "Fell Scythe",
+          description: `Attacks 1 enemy. Has an extra 20% chance of inflicting a critical hit.<br><br>After attacking, transfers all debuffs from this Champion to the target. <br><br>If the target is under a [Hex] debuff before the attack, transfers all debuffs from this Champion to the target before attacking instead.`,
+          damage: "4*ATK",
+          levelInfo: ["Level 2: Damage +20%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort5.webp",
+          name: "Final Testament",
+          description: `Attacks all enemies. Has an extra 20% chance of inflicting a critical hit. Before attacking, removes all debuffs from this Champion. <br><br>Will ignore 20% of each enemy's DEF. Will ignore 35% of each target's DEF instead if they are under a [Hex] debuff.<br><br>Damage increases by 10% for each debuff on each target.`,
+          damage: "4.2*ATK*(1+0.1*Target Debuffs)",
+          cooldown: 4,
+          levelInfo: ["Level 2: Damage +20%", "Level 3: Cooldown -1"],
+          isPassive: false
+        },
+        {
+          img: "assets/sort6.webp",
+          name: "Termination",
+          description: `Attacks all enemies. Has an extra 20% chance of inflicting a critical hit. Before attacking, places a [Hex] debuff and a 60% [Decrease DEF] debuff for 2 turns. <br><br>Will ignore [Life Barrier], [Poison Cloud], and [Ally Protection] buffs.`,
+          damage: "4.4*ATK",
+          cooldown: 3,
+          levelInfo: ["Level 2: Damage +20%", "Level 3: Ignore RES +20%"],
+          isPassive: false
+        },
+        {
+          img: "assets/meta2.webp",
+          name: "Metamorph",
+          description: `Transforms this Champion into their Base Form. Then grants an Extra Turn.`,
+          cooldown: 4,
+          isPassive: false
+        },
+        {
+          img: "assets/passif2.webp",
+          name: "Banquet of Death [P]",
+          description: `Whenever this Champion attacks an enemy under a [Hex] debuff, heals this Champion by 30% of the damage inflicted.<br><br>Also increases this Champion's ATK by 20% for each enemy under a [Hex] debuff.<br><br>Heal Multiplier: 0.3*Damage Dealt`,
+          isPassive: true
+        }
+      ],
+    stats: {
+        "PV": "16 845",
+        "ATQ": "1 553",
+        "DEF": "1 178",
+        "VIT": "105",
+        "TAUX C.": "15%",
+        "DÉG C.": "63%",
+        "RÉS": "30",
+        "PRÉ": "20"
+      },
+    aura: {
+    img: "../../../../../assets/images/auras/attack.webp",
+    description: `Increases Ally ATK in All Battles by 35%`,
+  },
+  },
+};

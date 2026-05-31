da95aff41e83f59541c2a95404c0834b5a0b745e
PGBmax <levinounet@gmail.com>
2026-05-31 05:09:06 +0200

    include: common/warboy/warboy.js

---
+++ pages/factions/faction08/common/warboy/warboy.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Défense",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Plunder",
+          description: `Attacks 1 enemy. Has a 15% chance of placing a [Provoke] debuff for 1 turn.`,
+          damage: "3.5*DEF",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Damage +5%", "Level 5: Damage +5%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "13 215",
+        "ATQ": "584",
+        "DEF": "738",
+        "VIT": "91",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

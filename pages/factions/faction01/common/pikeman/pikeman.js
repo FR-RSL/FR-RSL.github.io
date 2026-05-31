7bc670c818fe0c3e5bdaa0f0e056f024d3cf5fe5
PGBmax <levinounet@gmail.com>
2026-05-31 05:06:10 +0200

    build: common/pikeman/pikeman.js

---
+++ pages/factions/faction01/common/pikeman/pikeman.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Défense",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Run Through",
+          description: `Attacks 1 enemy. Has a 15% chance of placing a [Freeze] debuff for 1 turn.`,
+          damage: "3.4*ATK+25",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Buff/Debuff Chance +10%", "Level 5: Damage +10%", "Level 6: Buff/Debuff Chance +10%", "Level 7: Damage +10%", "Level 8: Buff/Debuff Chance +15%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "11 235",
+        "ATQ": "595",
+        "DEF": "859",
+        "VIT": "96",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

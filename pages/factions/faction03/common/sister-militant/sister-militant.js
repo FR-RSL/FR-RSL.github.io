2dce19a90cf0faf478c381eb1daee0f1f10c441e
PGBmax <levinounet@gmail.com>
2026-05-31 05:06:43 +0200

    build: common/sister-militant/sister-militant.js

---
+++ pages/factions/faction03/common/sister-militant/sister-militant.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Wide Swing",
+          description: `Attacks all enemies.`,
+          damage: "3.5*ATK",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Damage +5%", "Level 5: Damage +5%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "10 410",
+        "ATQ": "870",
+        "DEF": "639",
+        "VIT": "98",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

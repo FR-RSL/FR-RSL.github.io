0e93b6f5db7ed030357a9f046db1a3153ffc10a1
PGBmax <levinounet@gmail.com>
2026-05-31 05:06:43 +0200

    asset: common/novitiate/novitiate.js

---
+++ pages/factions/faction03/common/novitiate/novitiate.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Harrying Shot",
+          description: `Attacks 1 enemy. Has a 10% chance of placing a 30% [Decrease DEF] debuff for 2 turns.`,
+          damage: "2.7*ATK",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Buff/Debuff Chance +5%", "Level 5: Damage +5%", "Level 6: Buff/Debuff Chance +10%", "Level 7: Damage +10%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "10 245",
+        "ATQ": "892",
+        "DEF": "628",
+        "VIT": "83",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

6cb445e8fcaaffa54d3e74af6bd13c11e9ba062a
PGBmax <levinounet@gmail.com>
2026-05-31 05:10:07 +0200

    feat: common/hellhound/hellhound.js

---
+++ pages/factions/faction10/common/hellhound/hellhound.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Paralyzing Bite",
+          description: `Attacks 1 enemy. Places a [Sleep] debuff on the target for 1 turn when hit with a critical hit.`,
+          damage: "2.9*ATK+2.1*DEF",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Damage +5%", "Level 4: Damage +5%", "Level 5: Damage +5%", "Level 6: Damage +10%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "10 245",
+        "ATQ": "760",
+        "DEF": "760",
+        "VIT": "94",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;

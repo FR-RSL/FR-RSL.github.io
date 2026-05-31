b30328f866dcb555fcca1bbff9e338b938605f62
PGBmax <levinounet@gmail.com>
2026-05-31 05:06:43 +0200

    chore: common/yeoman/yeoman.js

---
+++ pages/factions/faction03/common/yeoman/yeoman.js
@@ -0,0 +1,25 @@
+const championData = {
+  type: "Attaque",
+  spells: [
+        {
+          img: "assets/sort1.webp",
+          name: "Volley",
+          description: `Attacks 1 enemy 3 times. Each hit has a 10% chance to decrease the Turn Meter by 20%.`,
+          damage: "ATK*1",
+          levelInfo: ["Level 2: Damage +5%", "Level 3: Buff/Debuff Chance +5%", "Level 4: Damage +5%", "Level 5: Buff/Debuff Chance +10%"],
+          isPassive: false
+        }
+      ],
+  stats: {
+        "PV": "9 090",
+        "ATQ": "903",
+        "DEF": "694",
+        "VIT": "97",
+        "TAUX C.": "15%",
+        "DÉG C.": "50%",
+        "RÉS": "30",
+        "PRÉ": "0"
+      },
+};
+
+const aura = null;
